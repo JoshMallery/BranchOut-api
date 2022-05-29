72280e803e235f8fadf0e0b368ef8c7a105c886b
Whitney Perricone <whitney.perricone@gmail.com>
2022-05-29 16:27:37 -0600

    Add data to test with

---
+++ database/data/coursesData.js
@@ -0,0 +1,14 @@
+let coursesData = [{
+  author: 'Brittany',
+  title: 'Lorem Ipsum',
+  overview: 'lesson here',
+  lessons: [{lesson_title: 'lesson one', lesson_content: 'blah, blah, blah'}]
+},
+{
+  author: 'Robbie',
+  title: 'Lesson title',
+  overview: 'lesson from Robbie here',
+  lessons: [{lesson_title: 'lesson two', lesson_content: 'content here'}, {lesson_title: 'lesson three', lesson_content: 'content lesson three'}]
+}]
+
+module.exports = coursesData;
